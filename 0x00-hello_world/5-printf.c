c8d5b0b7c38fade2547000f72631ad3a5806f4c1
Ubuntu <solomonadeborna@gmail.com>
2022-12-29 18:16:32 -0500

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+*main - A program that prints line using printf function
+*Return: 0 (Success)
+*/
+
+#include <stdio.h>
+int main(void)
+
+{
+puts("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
