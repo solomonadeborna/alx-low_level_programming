ab64a8254c774756e284c25c773a76f01964e917
Ubuntu <solomonadeborna@gmail.com>
2022-12-29 15:59:20 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,12 @@
 *followed by a new line"
 *return 0
 */
+
 #include <stdio.h>
 int main(void)
+
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
+
 }
