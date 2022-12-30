c9eebc1b8556387ad53c12f9959f81d3b79657ca
Ubuntu <solomonadeborna@gmail.com>
2022-12-30 14:01:39 -0500

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - prints whether the number stored in the variable n is positive, zero or negative
+*return: 0 (success)
+*/
+
+int main(void)
+
+{
+
+int n;
+
+srand(time(0));
+
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+{
+printf("%d is positive\n", n);
+
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+
+}
+return (0);
+
+}
