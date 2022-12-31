777f9be8faccdb0e848d270e18a4b91c41621ddc
Ubuntu <solomonadeborna@gmail.com>
2022-12-30 20:09:49 -0500

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,19 +18,19 @@ if (LD > 5)
 {
 printf("%d last digit of \n", n);
 printf("%d is \n", LD);
-printf("%d and is greater than 5\n");
+printf("and is greater than 5\n");
 }
 else if (LD == 0)
 {
 printf("%d last digit of \n", n);
 printf("%d is \n", LD);
-printf("%d and is 0 \n");
+printf("and is 0 \n");
 }
 else if (LD < 6)
 {
 printf("%d last digit of \n", n);
 printf("%d is \n", LD);
-printf("%d and is less than 6 and not 0 \n");
+printf("and is less than 6 and not 0 \n");
 }
 
 return (0);
