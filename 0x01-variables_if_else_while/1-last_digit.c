569f28da7a92184d088593df8f32b60a228b2b15
Ubuntu <solomonadeborna@gmail.com>
2022-12-30 20:19:59 -0500

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,21 +16,18 @@ n = rand() - RAND_MAX / 2;
 LD = n % 10;
 if (LD > 5)
 {
-printf("last digit of %d \n", n);
-printf("is %d \n", LD);
-printf("and is greater than 5\n");
+printf("last digit of %d is %d and is greater than 5 \n", n, LD);
+
 }
 else if (LD == 0)
 {
-printf("last digit of %d \n", n);
-printf("is %d \n", LD);
-printf("and is 0 \n");
+printf("last digit of %d is %d and is 0 \n", n);
+
 }
 else if (LD < 6)
 {
-printf("last digit of %d \n", n);
-printf("is %d\n", LD);
-printf("and is less than 6 and not 0 \n");
+printf("last digit of %d is %d and is less than 6 and not 0 \n", n, LD);
+
 }
 
 return (0);
