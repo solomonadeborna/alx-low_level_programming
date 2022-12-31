c2bc94c37454220359ed8ec4d772db63564833f0
Ubuntu <solomonadeborna@gmail.com>
2022-12-31 12:15:36 -0500

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*main - prints the alphabet in lowercase
+*followed by a new line
+*Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+
+_putchar(c);
+
+}
+_putchar('\n');
+
+}
