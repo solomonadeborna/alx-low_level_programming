f9bd6b1f024414eff450e98432e0e8bc15f93589
Ubuntu <solomonadeborna@gmail.com>
2023-01-02 15:02:15 -0500

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/**
-*while (i < 10) - cause of infinite loop
-*{
-*putchar(i);
-*}
-*/
+
+/*while (i < 10) - cause of infinite loop*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
