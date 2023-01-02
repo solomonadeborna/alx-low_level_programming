1be990ac99bc16629cf7983a80b68f2ea6707968
Ubuntu <solomonadeborna@gmail.com>
2023-01-02 14:58:16 -0500

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - causes an infinite loop and print on new line
 * Return: 0
 */
 
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /**
-*while (i < 10)
+*while (i < 10) - cause of infinite loop
 *{
 *putchar(i);
-*}*/
-
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
