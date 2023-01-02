714782049118a70dcf155856a5269950aece9601
Ubuntu <solomonadeborna@gmail.com>
2023-01-02 16:46:50 -0500

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,17 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > a && b > c)
-{
-largest = b;
-}
-else if (c > a && c > b)
+else if (a > b && c > a)
 {
 largest = c;
 }
+else if (b > c)
+{
+largest = b;
+}
 else
 {
-largest =a;
+largest =c;
 }
 return (largest);
 }
