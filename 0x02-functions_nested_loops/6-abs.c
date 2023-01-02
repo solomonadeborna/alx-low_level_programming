7be945df6eb970dbd379a2c9e85b8c5701d951da
Ubuntu <solomonadeborna@gmail.com>
2023-01-01 22:05:26 -0500

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_abs - function that computes the absolute value of an integer.
+*@n: the number to be checked
+*Return: absolute value of number or zero
+*/
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+
+int abs_val;
+abs_val = n * -1;
+return (abs_val);
+}
+return (n);
+}
