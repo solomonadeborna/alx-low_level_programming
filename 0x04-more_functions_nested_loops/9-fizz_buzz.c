53640a7def7f6b03cec4aeeeb031769aff5a6001
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 07:42:48 -0500

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - prints Fizz Buzz program
+**Return: 0 always (success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+
+return (0);
+}
