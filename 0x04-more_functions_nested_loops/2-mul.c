d6357834e1c1ff42a8f148680a80334c0eb3e282
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 04:36:05 -0500

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_isdigit- checks if character is a digit
+*@c: the character to be checked
+*Return: 1 if c is a digit and 0 for anything else
+*/
+
+int mul(int a, int b)
+{
+
+return (a * b);
+
+
+}
