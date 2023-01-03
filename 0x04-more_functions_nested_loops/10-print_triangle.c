021e9dee647ce7400aedad3d24aa668b9c1d80f7
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 08:07:53 -0500

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*print_triangle - prints triangles
+*@size: size of the triangle
+*Return: empty
+*/
+
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = size - i; j > 1; j--)
+{
+_putchar(32);
+}
+for (k = 0; i <= i; i++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
