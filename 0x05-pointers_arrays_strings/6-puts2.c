eb9d56c82372efb82d1e582da2466a08c70fa183
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 12:00:57 -0500

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,18 +3,19 @@
 /**
 *puts2 - Prints one char out of two of a string.
 *@s: The string under consideration.
-*Return: void
+*Return: nothing
 */
 void puts2(char *s)
 {
-int index = 0;
-len = 0;
-
-while (s[index++])
-len++;
-
-for (index = 0; index < len; index += 2)
-_putchar(s[index]);
-
+int i, int j;
+j = 0;
+while (s[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < j; i += 2)
+{
+_putchar(s[i]);
+}
 _putchar('\n');
 }
