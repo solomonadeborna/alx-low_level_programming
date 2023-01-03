e4d72e391abac1a44a5f84b24c7d60ecfd744bbc
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 14:33:24 -0500

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* reverse_array - reverse the contents of integer array
+* @a: array of integers
+* @n: number of elements in array
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+for (i = 0; i < (n / 2); i++)
+{
+j = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = j;
+}
+}
