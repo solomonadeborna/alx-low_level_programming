14986f2d40bc898db75ca12c790c7cc6525e90f1
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 11:54:08 -0500

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*puts2 - Prints one char out of two of a string.
+*@s: The string under consideration.
+*Return: nothing
+*/
+void puts2(char *s)
+{
+int index = 0;
+len = 0;
+
+while (s[index++])
+len++;
+
+for (index = 0; index < len; index += 2)
+_putchar(s[index]);
+
+_putchar('\n');
+}
