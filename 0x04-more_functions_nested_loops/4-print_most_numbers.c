661d490cf5c8fc89fd7770cc30528596eebbc560
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 05:09:03 -0500

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_most_numbers- prints the numbers, from 0 to 9
+*except 2 and 4
+*followed by new line
+*Return: the numbers from 0 to 9
+*/
+
+void print_most_numbers(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+if ((x != 2) && (x != 4))
+{
+_putchar(x + '0');
+}
+}
+_putchar('\n');
+}
