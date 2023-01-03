4cf45dd788a472f3d178c536bc970dd688be30a8
Ubuntu <solomonadeborna@gmail.com>
2023-01-03 04:56:02 -0500

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers- prints the numbers, from 0 to 9
+*followed by new line
+*Return: the numbers from 0 to 9
+*/
+
+int print_numbers(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+_putchar(x);
+
+}
+_putchar('\n');
+}
